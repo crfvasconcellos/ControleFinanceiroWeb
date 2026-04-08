420a99a0edab11abc90e00d2016bdf3a5fa1c130
JurRter <brendo.henrique@mail.uft.edu.br>
2026-04-08 09:29:17 -0300

    Ajustando o CSS com o INDEX e adicionando a US01 ao projeto!

--- Projeto/public/index.php
+++ Projeto/public/index.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Despesa - Controle Financeiro</title>
-    <link rel="stylesheet" href="/Projeto/assets/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
